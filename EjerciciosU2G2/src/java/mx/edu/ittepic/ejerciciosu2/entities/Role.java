718c4ed82d5008bf81fb70158ce4c3161dbe6d79
Arcadia <chalarcadiaca@ittepic.edu.mx>
2016-09-15 08:30:02 -0600

    EjerciciosU2G2_version1
    Creacion del proyecto

---
+++ EjerciciosU2G2/src/java/mx/edu/ittepic/ejerciciosu2/entities/Role.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ittepic.ejerciciosu2.entities;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Christian alberto arcadia
+ */
+@Entity
+@Table(name = "role")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Role.findAll", query = "SELECT r FROM Role r"),
+    @NamedQuery(name = "Role.findByIdrole", query = "SELECT r FROM Role r WHERE r.idrole = :idrole"),
+    @NamedQuery(name = "Role.findByRole", query = "SELECT r FROM Role r WHERE r.role = :role")})
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "idrole")
+    private Integer idrole;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 30)
+    @Column(name = "role")
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(Integer idrole) {
+        this.idrole = idrole;
+    }
+
+    public Role(Integer idrole, String role) {
+        this.idrole = idrole;
+        this.role = role;
+    }
+
+    public Integer getIdrole() {
+        return idrole;
+    }
+
+    public void setIdrole(Integer idrole) {
+        this.idrole = idrole;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idrole != null ? idrole.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Role)) {
+            return false;
+        }
+        Role other = (Role) object;
+        if ((this.idrole == null && other.idrole != null) || (this.idrole != null && !this.idrole.equals(other.idrole))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ittepic.ejerciciosu2.entities.Role[ idrole=" + idrole + " ]";
+    }
+    
+}
